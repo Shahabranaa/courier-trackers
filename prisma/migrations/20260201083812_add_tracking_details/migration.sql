058868c5e8ce5627c5ac8c8f610aae1298cc4359
Backlinks Tracker <shahabranaa@gmail.com>
2026-02-09 06:28:22 +0500

    chore: Switch from SQLite to PostgreSQL for Vercel deployment
    - Updated prisma/schema.prisma to use postgresql provider
    - Removed old SQLite migrations
    - Added .env.example for Vercel Postgres

--- prisma/migrations/20260201083812_add_tracking_details/migration.sql
+++
@@ -1,7 +0,0 @@
--- AlterTable
-ALTER TABLE "Order" ADD COLUMN "actualWeight" REAL;
-ALTER TABLE "Order" ADD COLUMN "lastStatus" TEXT;
-ALTER TABLE "Order" ADD COLUMN "lastStatusTime" DATETIME;
-ALTER TABLE "Order" ADD COLUMN "transactionFee" REAL;
-ALTER TABLE "Order" ADD COLUMN "transactionTax" REAL;
-ALTER TABLE "Order" ADD COLUMN "upfrontPayment" REAL;
--- prisma/migrations/20260201085703_add_reversal_fields/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Order" ADD COLUMN "reversalFee" REAL;
-ALTER TABLE "Order" ADD COLUMN "reversalTax" REAL;
